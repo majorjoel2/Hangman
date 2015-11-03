8f221b3a80b0ddc4145f407ab02705fe308376e7
majorjoel2 <joel@tanner-family.org>
2015-11-03 08:58:58 -0500

    Added the code I have so far

--- Hangman/src/MainHangman.java
+++ Hangman/src/MainHangman.java
@@ -3,6 +3,26 @@ import java.util.Scanner;
 
 public class MainHangman {
 	
+	public static String randomWord() {
+		//select a word at random
+		String[] wordList = new String[] {"", "1", "2"};
+		int min = 0;
+		int max = 9;
+		Random Random = new Random();
+		int randomWord = Random.nextInt(max - min + 1) + min;
+		String output = wordList[randomWord];
+		return output;
+	}
+	
+	public static int prepareHangman(String word) {
+		//do some stuff to prepare for the game
+		return word.length();
+	}
+	
+	public static byte askLetter(String word) {
+		return 1;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
